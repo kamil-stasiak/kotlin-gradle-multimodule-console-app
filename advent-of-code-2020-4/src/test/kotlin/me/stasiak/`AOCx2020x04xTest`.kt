6cee25c31017a8c71a98c5d2b6484f34665a61d2
Kamil Stasiak <kamilstasiaknew@gmail.com>
2021-09-09 21:54:21 +0200

    Refactor AOC 4

--- advent-of-code-2020-4/src/test/kotlin/me/stasiak/`AOCx2020x04xTest`.kt
+++ advent-of-code-2020-4/src/test/kotlin/me/stasiak/`AOCx2020x04xTest`.kt
@@ -23,87 +23,83 @@ sealed class Field(
     }
 
     class Hgt(value: String) : Field(value = value) {
-        override fun isValid(): Boolean {
-            // a number followed by either cm or in:
-            return when {
-                // If cm, the number must be at least 150 and at most 193.
-                value.endsWith("cm") -> value.substringBefore("cm").toIntOrNull() in (150..193)
-                // If in, the number must be at least 59 and at most 76.
-                value.endsWith("in") -> value.substringBefore("in").toIntOrNull() in (59..76)
-                else -> false
-            }
+        // a number followed by either cm or in:
+        override fun isValid(): Boolean = when (value.takeLast(2)) {
+            // If cm, the number must be at least 150 and at most 193.
+            "cm" -> value.removeSuffix("cm").toIntOrNull() in (150..193)
+            // If in, the number must be at least 59 and at most 76.
+            "in" -> value.removeSuffix("in").toIntOrNull() in (59..76)
+            else -> false
         }
     }
 
     class Hcl(value: String) : Field(value = value) {
+        companion object {
+            private val REGEX = """#[a-f0-9]{6}""".toRegex()
+        }
+
+        // a # followed by exactly six characters 0-9 or a-f.
         override fun isValid(): Boolean {
-            // a # followed by exactly six characters 0-9 or a-f.
-            val hex = value.substring(1)
-            return value.startsWith("#")
-                    && hex.length == 6
-                    && hex matches "[a-f0-9]*".toRegex()
+            return value matches REGEX
         }
     }
 
     class Ecl(value: String) : Field(value = value) {
-        override fun isValid(): Boolean {
-            // exactly one of: amb blu brn gry grn hzl oth.
-            return listOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth").contains(value)
+        companion object {
+            private val VALID_VALUES = listOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
         }
+
+        // exactly one of: amb blu brn gry grn hzl oth.
+        override fun isValid(): Boolean = VALID_VALUES.contains(value)
     }
 
     class Pid(value: String) : Field(value = value) {
-        override fun isValid(): Boolean {
-            // a nine-digit number, including leading zeroes.
-            val number = value.toBigDecimalOrNull()
-            Integer.MAX_VALUE
-            return number != null && value.length == 9
-        }
+        // a nine-digit number, including leading zeroes.
+        override fun isValid(): Boolean = value.toBigDecimalOrNull() != null && value.length == 9
     }
 
     class Cid(value: String) : Field(value = value) {
-        override fun isValid(): Boolean {
-            return false
-        }
+        override fun isValid(): Boolean = false
     }
 
     abstract fun isValid(): Boolean
 }
 
-
 class AOCx2020x04xTest {
-    val requiredFileds = listOf(
-        "byr",
-        "iyr",
-        "eyr",
-        "hgt",
-        "hcl",
-        "ecl",
-        "pid",
-    )
+    companion object {
+        val REQUIRED_FIELDS = listOf(
+            "byr",
+            "iyr",
+            "eyr",
+            "hgt",
+            "hcl",
+            "ecl",
+            "pid",
+        )
+    }
 
     @Test
     fun `1  on example data`() {
         val input = """
-ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
-byr:1937 iyr:2017 cid:147 hgt:183cm
-
-iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
-hcl:#cfa07d byr:1929
-
-hcl:#ae17e1 iyr:2013
-eyr:2024
-ecl:brn pid:760753108 byr:1931
-hgt:179cm
-
-hcl:#cfa07d eyr:2025 pid:166559648
-iyr:2011 ecl:brn hgt:59in
+            ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+            byr:1937 iyr:2017 cid:147 hgt:183cm
+            
+            iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+            hcl:#cfa07d byr:1929
+            
+            hcl:#ae17e1 iyr:2013
+            eyr:2024
+            ecl:brn pid:760753108 byr:1931
+            hgt:179cm
+            
+            hcl:#cfa07d eyr:2025 pid:166559648
+            iyr:2011 ecl:brn hgt:59in
         """.trimIndent()
             .split("\n\n")
 
         val result = input
             .map { it.replace("\n", " ") } // every line
-            .filter { passport -> requiredFileds.all { fieldName -> passport.contains(fieldName) } }
+            .filter { passport -> REQUIRED_FIELDS.all { fieldName -> passport.contains(fieldName) } }
 
         assertEquals(2, result.size, "Error!")
     }
@@ -118,7 +114,7 @@ iyr:2011 ecl:brn hgt:59in
             .filter { fields ->
                 fields
                     .filter { field ->
-                        requiredFileds.any { req ->
+                        REQUIRED_FIELDS.any { req ->
                             field.startsWith(req)
                         }
                     }.size == 7
@@ -130,19 +126,19 @@ iyr:2011 ecl:brn hgt:59in
     @Test
     fun `2  on example valid data`() {
         val input = """
-pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
-hcl:#623a2f
-
-eyr:2029 ecl:blu cid:129 byr:1989
-iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
-
-hcl:#888785
-hgt:164cm byr:2001 iyr:2015 cid:88
-pid:545766238 ecl:hzl
-eyr:2022
-
-iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
-        """.trimIndent()
+            pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+            hcl:#623a2f
+            
+            eyr:2029 ecl:blu cid:129 byr:1989
+            iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+            
+            hcl:#888785
+            hgt:164cm byr:2001 iyr:2015 cid:88
+            pid:545766238 ecl:hzl
+            eyr:2022
+            
+            iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+            """.trimIndent()
             .split("\n\n")
 
         val result = input
@@ -159,20 +155,20 @@ iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
     @Test
     fun `2  on example invalid data`() {
         val input = """
-eyr:1972 cid:100
-hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
-
-iyr:2019
-hcl:#602927 eyr:1967 hgt:170cm
-ecl:grn pid:012533040 byr:1946
-
-hcl:dab227 iyr:2012
-ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
-
-hgt:59cm ecl:zzz
-eyr:2038 hcl:74454a iyr:2023
-pid:3556412378 byr:2007
-""".trimIndent()
+            eyr:1972 cid:100
+            hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+            
+            iyr:2019
+            hcl:#602927 eyr:1967 hgt:170cm
+            ecl:grn pid:012533040 byr:1946
+            
+            hcl:dab227 iyr:2012
+            ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+            
+            hgt:59cm ecl:zzz
+            eyr:2038 hcl:74454a iyr:2023
+            pid:3556412378 byr:2007
+            """.trimIndent()
             .split("\n\n")
 
         val result = input
